bb9d3cb981a7997321cf03b71fe88060c8639f0d
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-07 22:00:21 -0500

    Adding sounds to each letter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-for(var i = 0; i<document.querySelector(".drum").lenght; i++){
+
+for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got click");
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     });
 }
